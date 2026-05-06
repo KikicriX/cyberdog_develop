973983c3e253c2b507535267d155f9f377af9407
KikicriX <1755130787@qq.com>
2026-05-06 17:33:23 +0800

    Add files via upload

---
+++ launchgazebo_lidar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /opt/ros/galactic/setup.bash
+source install/setup.bash
+chmod +x src/cyberdog_simulator/cyberdog_gazebo/script/gazebolauncher.py
+
+python3 src/cyberdog_simulator/cyberdog_gazebo/script/gazebolauncher.py ros2 launch cyberdog_gazebo race_gazebo.launch.py use_lidar:=true
